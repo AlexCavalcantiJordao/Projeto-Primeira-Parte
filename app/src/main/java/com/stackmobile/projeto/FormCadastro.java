db5462f7742cbf90e42a0984167ef46f85a75996
AlexCavalcantiJordao <alex37705732@gmail.com>
2024-01-04 13:31:33 -0300

    app/ gradle.properties my-Projeto/ projeto/ app.json gradlew* my-app/ settings.gradle.kts build.gradle.kts gradlew.bat package-lock.json gradle/ local.properties package.json

---
+++ app/src/main/java/com/stackmobile/projeto/FormCadastro.java
@@ -0,0 +1,19 @@
+package com.stackmobile.projeto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class FormCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_cadastro);
+
+        getSupportActionBar().hide();
+    }
+}
